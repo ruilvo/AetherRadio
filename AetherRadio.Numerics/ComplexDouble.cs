3110f8455dc95ec2f765525c6dd24455fa9b5ace
Rui Oliveira <ruimail24@gmail.com>
2023-04-20 10:27:24 +0100

    Add Complex numerics

---
+++ AetherRadio.Numerics/ComplexDouble.cs
@@ -0,0 +1,71 @@
+﻿namespace AetherRadio.Numerics;
+
+public struct ComplexDouble
+{
+    double _real;
+    double _imag;
+
+    public double Real
+    {
+        get { return _real; }
+        set { _real = value; }
+    }
+
+    public double Imaginary
+    {
+        get { return _imag; }
+        set { _imag = value; }
+    }
+
+    public double Magnitude
+    {
+        get { return Math.Sqrt(_real * _real + _imag * _imag); }
+    }
+
+    public double Phase
+    {
+        get { return Math.Atan2(_imag, _real); }
+    }
+
+    public ComplexDouble Normalized
+    {
+        get { return this / FromPolar(Magnitude, 0); }
+    }
+
+    public ComplexDouble(double real, double imag)
+    {
+        _real = real;
+        _imag = imag;
+    }
+
+    public static ComplexDouble FromPolar(double magnitude, double phase)
+    {
+        return new ComplexDouble(magnitude * Math.Cos(phase), magnitude * Math.Sin(phase));
+    }
+
+    public static ComplexDouble operator +(ComplexDouble a, ComplexDouble b)
+    {
+        return new ComplexDouble(a.Real + b.Real, a.Imaginary + b.Imaginary);
+    }
+
+    public static ComplexDouble operator -(ComplexDouble a, ComplexDouble b)
+    {
+        return new ComplexDouble(a.Real - b.Real, a.Imaginary - b.Imaginary);
+    }
+
+    public static ComplexDouble operator *(ComplexDouble a, ComplexDouble b)
+    {
+        return FromPolar(a.Magnitude * b.Magnitude, a.Phase + b.Phase);
+    }
+
+    public static ComplexDouble operator /(ComplexDouble a, ComplexDouble b)
+    {
+        return FromPolar(a.Magnitude / b.Magnitude, a.Phase - b.Phase);
+    }
+
+
+    public void Normalize()
+    {
+        this = Normalized;
+    }
+}
---
+++ AetherRadio.Numerics/ComplexFloat.cs
@@ -0,0 +1,71 @@
+﻿namespace AetherRadio.Numerics;
+
+public struct ComplexFloat
+{
+    float _real;
+    float _imag;
+
+    public float Real
+    {
+        get { return _real; }
+        set { _real = value; }
+    }
+
+    public float Imaginary
+    {
+        get { return _imag; }
+        set { _imag = value; }
+    }
+
+    public float Magnitude
+    {
+        get { return MathF.Sqrt(_real * _real + _imag * _imag); }
+    }
+
+    public float Phase
+    {
+        get { return MathF.Atan2(_imag, _real); }
+    }
+
+    public ComplexFloat Normalized
+    {
+        get { return this / FromPolar(Magnitude, 0); }
+    }
+
+    public ComplexFloat(float real, float imag)
+    {
+        _real = real;
+        _imag = imag;
+    }
+
+    public static ComplexFloat FromPolar(float magnitude, float phase)
+    {
+        return new ComplexFloat(magnitude * MathF.Cos(phase), magnitude * MathF.Sin(phase));
+    }
+
+    public static ComplexFloat operator +(ComplexFloat a, ComplexFloat b)
+    {
+        return new ComplexFloat(a.Real + b.Real, a.Imaginary + b.Imaginary);
+    }
+
+    public static ComplexFloat operator -(ComplexFloat a, ComplexFloat b)
+    {
+        return new ComplexFloat(a.Real - b.Real, a.Imaginary - b.Imaginary);
+    }
+
+    public static ComplexFloat operator *(ComplexFloat a, ComplexFloat b)
+    {
+        return FromPolar(a.Magnitude * b.Magnitude, a.Phase + b.Phase);
+    }
+
+    public static ComplexFloat operator /(ComplexFloat a, ComplexFloat b)
+    {
+        return FromPolar(a.Magnitude / b.Magnitude, a.Phase - b.Phase);
+    }
+
+
+    public void Normalize()
+    {
+        this = Normalized;
+    }
+}
