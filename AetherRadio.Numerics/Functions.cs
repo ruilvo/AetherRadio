3396b0bb505d17f7b8e365daa461d73e2633d2f0
Rui Oliveira <ruimail24@gmail.com>
2023-04-22 19:29:15 +0100

    Fix IsPowerOfTwo

--- AetherRadio.Numerics/Functions.cs
+++ AetherRadio.Numerics/Functions.cs
@@ -4,7 +4,7 @@ public static class Functions
 {
     public static bool IsPowerOfTwo(uint value)
     {
-        return (value & (value - 1)) == 0;
+        return value != 0 && (value & (value - 1)) == 0;
     }
 }
 
