f87d412d186b3cad51a1753e39c400700c2404f5
Rui Oliveira <ruimail24@gmail.com>
2023-04-22 14:13:26 +0100

    Add a project to perform FFTs

---
+++ AetherRadio.Fft/FftFloat.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+using AetherRadio.Numerics;
+namespace AetherRadio.Fft;
+
+public class FftFloat
+{
+    private readonly uint _length;
+    private readonly ComplexFloat[] _twiddleFactors;
+
+    public FftFloat(uint length)
+    {
+        Debug.Assert(Numerics.Math.IsPowerOfTwo(length), 
+            "The FFT needs to be a power of two!");
+
+        _length = length;
+        _twiddleFactors = new ComplexFloat[length / 2];
+
+        // Compute the twiddle factors
+        for (uint i = 0; i < _twiddleFactors.Length; i++)
+        {
+            var angle = -2.0f * MathF.PI * i / _length;
+            _twiddleFactors[i] = new ComplexFloat(
+                               MathF.Cos(angle), MathF.Sin(angle));
+        }
+    }
+
+    public void Transform(ReadOnlySpan<ComplexFloat> input, Span<ComplexFloat> output)
+    {
+        Debug.Assert(input.Length == _length);
+        Debug.Assert(output.Length == _length);
+
+        // Copy the input to the output, bit-reversed
+        CopyBitReversed(input, output);
+
+        // Perform the FFT
+        TransformInternal(output);
+    }
+
+    // TODO(ruilvo): Implement CopyBitReversed and TransformInternal.
+}
