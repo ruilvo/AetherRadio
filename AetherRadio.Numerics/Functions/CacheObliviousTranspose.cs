5b9dffd4705a611c83b129f90fda74480df9ee37
Rui Oliveira <ruimail24@gmail.com>
2023-05-12 17:37:29 +0100

    Small cleanup

--- AetherRadio.Numerics/Functions/CacheObliviousTranspose.cs
+++ AetherRadio.Numerics/Functions/CacheObliviousTranspose.cs
@@ -17,12 +17,10 @@ public static partial class Functions
         // Get the dimensions of the input and output spans
         var rows = input.Height;
         var cols = input.Width;
-        var rowsOut = cols; // = output.Height;
-        var colsOut = rows; // = output.Width;
 
         // If the input or output spans are too small, use a simple loop to transpose them
-        const int threshold = 32;
-        if (rows * cols <= threshold || rowsOut * colsOut <= threshold)
+        const int threshold = 16;
+        if (rows * cols <= threshold)
         {
             for (var i = 0; i < rows; i++)
             {
@@ -37,12 +35,10 @@ public static partial class Functions
         // Otherwise, recursively split the input and output spans into four sub-spans and transpose them
         var halfRows = rows / 2;
         var halfCols = cols / 2;
-        var halfRowsOut = halfCols; // =  rowsOut / 2;
-        var halfColsOut = halfRows; // colsOut / 2;
 
-        CacheObliviousTranspose(input[..halfRows, ..halfCols], output[..halfColsOut, ..halfRowsOut]); // Top-left quadrant
-        CacheObliviousTranspose(input[..halfRows, halfCols..], output[halfColsOut.., ..halfRowsOut]); // Top-right quadrant
-        CacheObliviousTranspose(input[halfRows.., ..halfCols], output[..halfColsOut, halfRowsOut..]); // Bottom-left quadrant
-        CacheObliviousTranspose(input[halfRows.., halfCols..], output[halfColsOut.., halfRowsOut..]); // Bottom-right quadrant
+        CacheObliviousTranspose(input[..halfRows, ..halfCols], output[..halfRows, ..halfCols]); // Top-left quadrant
+        CacheObliviousTranspose(input[..halfRows, halfCols..], output[halfRows.., ..halfCols]); // Top-right quadrant
+        CacheObliviousTranspose(input[halfRows.., ..halfCols], output[..halfRows, halfCols..]); // Bottom-left quadrant
+        CacheObliviousTranspose(input[halfRows.., halfCols..], output[halfRows.., halfCols..]); // Bottom-right quadrant
     }
 }
